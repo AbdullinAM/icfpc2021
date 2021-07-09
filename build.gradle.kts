0f686eae9bf082c8bfa2babab42e7ad2562d3e83
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2021-07-09 14:59:07 +0300

    Test -> kotlin-test

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation(kotlin("test"))
 }
 
 tasks.getByName<Test>("test") {
