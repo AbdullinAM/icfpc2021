37d2d2d760f98ec228bef523a37ae692d4c3cc77
saloed <napster_1997@mail.ru>
2021-07-10 22:53:49 +0300

    random restarts

--- src/main/kotlin/ru/spbstu/icpfc2021/solver/otherDummy.kt
+++ src/main/kotlin/ru/spbstu/icpfc2021/solver/otherDummy.kt
@@ -29,11 +29,16 @@ fun Boolean.toInt() = when {
     else -> 0
 }
 
+enum class SolverMode {
+    SINGLE, RANDOM
+}
+
 class OtherDummySolver(
     val allHolePoints: Set<Point>,
     val problem: Problem,
     val findAllSolutions: Boolean = false,
-    val showGraphics: Boolean = false
+    val showGraphics: Boolean = false,
+    val mode: SolverMode = SolverMode.SINGLE
 ) {
     val verifier = Verifier(problem)
     val canvas: TransformablePanel
@@ -190,7 +195,51 @@ class OtherDummySolver(
             }
 
         println("Start search")
+        return when (mode) {
+            SolverMode.SINGLE -> singleTryMode()
+            SolverMode.RANDOM -> randomTriesMode()
+        }
+    }
 
+    private fun randomTriesMode(): Figure {
+        var result: Figure? = null
+        var tryIdx = 0
+        while (tryIdx++ < 100) {
+            println("Start try $tryIdx")
+            val initialCtx = randomInitialSeed()
+            val startingIdx = initialCtx.vertices.best() ?: return problem.figure
+            val tryResult = searchVertex(startingIdx, initialCtx.withVertex(startingIdx)) ?: continue
+            val tryFig = problem.figure.copy(vertices = tryResult.assigment.filterNotNull())
+            if (saveResult(problem, tryFig) || result == null) {
+                result = tryFig
+            }
+        }
+        return result ?: error("No solution found")
+    }
+
+    private fun randomInitialSeed(): VertexCtx {
+        val vertexAmount = problem.figure.vertices.size
+        val holeVertices = problem.hole.toMutableSet()
+        val assignments = MutableList<Point?>(vertexAmount) { null }
+        for (i in 0..minOf(vertexAmount, holeVertices.size, 5)) {
+            val emptyVertices = assignments.withIndex().filter { it.value == null }.map { it.index }
+            val vertex = emptyVertices.random()
+            val point = holeVertices.random()
+            holeVertices -= point
+            assignments[vertex] = point
+        }
+        val possiblePoints = MutableList(vertexAmount) { i ->
+            val assigment = assignments[i]
+            if (assigment == null) allHolePoints else setOf(assigment)
+        }
+        return VertexCtx(
+            possiblePoints.toPersistentList(),
+            assignments.toPersistentList(),
+            (0 until vertexAmount).toPersistentSet()
+        )
+    }
+
+    private fun singleTryMode(): Figure {
         val vctx = VertexCtx(
             MutableList(problem.figure.vertices.size) { allHolePoints }.toPersistentList(),
             MutableList(problem.figure.vertices.size) { null }.toPersistentList(),
@@ -199,8 +248,7 @@ class OtherDummySolver(
 
         val startingIdx = vctx.vertices.best() ?: return problem.figure
 
-        val result = searchVertex(startingIdx, vctx.withVertex(startingIdx))
-        if (result == null) error("No solution found")
+        val result = searchVertex(startingIdx, vctx.withVertex(startingIdx)) ?: error("No solution found")
         return problem.figure.copy(vertices = result.assigment.toList() as List<Point>)
     }
 
