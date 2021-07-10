1933e021f5ed2bccec3dc71bd56a9a4a517a33bb
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2021-07-10 12:46:59 +0300

    correct main

--- src/main/kotlin/ru/spbstu/icpfc2021/Main.kt
+++ src/main/kotlin/ru/spbstu/icpfc2021/Main.kt
@@ -1,7 +1,10 @@
 package ru.spbstu.icpfc2021
 
 import ru.spbstu.icpfc2021.gui.drawFigure
+import ru.spbstu.icpfc2021.model.Verifier
 import ru.spbstu.icpfc2021.model.readProblem
+import ru.spbstu.icpfc2021.result.saveResult
+import ru.spbstu.icpfc2021.solver.OtherDummySolver
 import java.io.File
 
 fun main(args: Array<String>) {
@@ -10,13 +13,13 @@ fun main(args: Array<String>) {
     println("$index.problem")
     val problem = readProblem(index, json)
     println(problem)
-//    val solver = OtherDummySolver(
-//        Verifier(problem).getHolePoints().toSet(),
-//        problem
-//    )
-//    val figure = solver.solve()
-//
-//    saveResult(problem, figure)
-//    drawFigure(problem, figure)
-    drawFigure(problem)
+    val solver = OtherDummySolver(
+        Verifier(problem).getHolePoints().toSet(),
+        problem
+    )
+    val figure = solver.solve()
+
+    saveResult(problem, figure)
+    drawFigure(problem, figure)
+//    drawFigure(problem)
 }
