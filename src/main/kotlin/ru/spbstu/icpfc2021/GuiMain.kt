2c3a8f7fcbf5c3652c744641e3c6a6fd8cea9d9a
Marat Akhin <marat.akhin@gmail.com>
2021-07-10 16:33:35 +0300

    Added GuiMain

---
+++ src/main/kotlin/ru/spbstu/icpfc2021/GuiMain.kt
@@ -0,0 +1,14 @@
+package ru.spbstu.icpfc2021
+
+import ru.spbstu.icpfc2021.gui.drawFigure
+import ru.spbstu.icpfc2021.model.readProblem
+import java.io.File
+
+fun main(args: Array<String>) {
+    val index = args[0].toInt()
+    val json = File("problems/$index.problem").readText()
+    println("$index.problem")
+    val problem = readProblem(index, json)
+    println(problem)
+    drawFigure(problem, problem.figure)
+}
