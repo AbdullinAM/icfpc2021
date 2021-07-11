c5206fa94016dc9ac4a7ec69544858504d41c811
Marat Akhin <marat.akhin@gmail.com>
2021-07-11 14:39:40 +0300

    post_single.sh

---
+++ post_single.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source api_key.sh
+source settings.sh
+
+try_post_problem_by_file $1
--- settings.sh
+++ settings.sh
@@ -17,3 +17,36 @@ function get_problem_current_id () {
 
     last_seen_uuid=$uuid
 }
+
+function try_post_problem_by_file () {
+	file=$1
+
+	filename="$(basename -- $file)"
+	extension="${filename##*.}"
+	problem_id="${filename%.*}"
+
+	echo "Getting current stats for problem $problem_id"
+	get_problem_current_id $problem_id
+
+	json=`curl -sL \
+	-X GET \
+	-H "Content-Type: application/json" \
+	-H "Authorization: Bearer $API_TOKEN" \
+	--data @"./$file" \
+	--url "https://poses.live/api/problems/$problem_id/solutions/$last_seen_uuid"`
+
+	current_dislikes=`echo $json | jq ".dislikes"`
+
+	solution_dislikes=`./gradlew -q -PmainClass="ru.spbstu.icpfc2021.SolutionsGraderKt" run --args $problem_id`
+
+	if [ $current_dislikes -gt $solution_dislikes ]; then
+	echo "We are better for $problem_id: $solution_dislikes < $current_dislikes";
+	echo "Posting solution for problem $problem_id"
+	curl -sL \
+	  -X POST \
+	  -H "Content-Type: application/json" \
+	  -H "Authorization: Bearer $API_TOKEN" \
+	  --data @"./$file" \
+	  --url "https://poses.live/api/problems/$problem_id/solutions"
+	fi
+}
